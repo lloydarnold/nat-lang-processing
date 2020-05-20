1bf5b522d81544931abc665a1ad6b75ff4d79dcb
Lloyd Arnold <lloarn@gmail.com>
2020-05-20 12:30:37 +0100

    Fix logic errors in cleanWords
    Absolute file paths cannot start with ~; they have to go all the way
    back to /home/

--- sentiment-analysis/preprocess/cleanWords.py
+++ sentiment-analysis/preprocess/cleanWords.py
@@ -1,23 +1,25 @@
 import os
 import re
+from pathlib import Path
 
-FILE_PATH = ""
+FILE_PATH = "/home/lloyd/Documents/programming/imperative/python/natlangprocessing/sentiment-analysis/data/test/testRaw1"
 CLEAN_FILE_PATH = FILE_PATH + "_clean"
 
 def clean_data(filePath = FILE_PATH, cleanFilePath = CLEAN_FILE_PATH):
     '''Remove all non-alphabetic characters.'''
 
-    with open(cleanFilePath, 'w') as writer:
+    with open(cleanFilePath, 'w+') as writer:
+        writer = open(cleanFilePath, 'w+')
         for line in open(filePath):
             line=line.rstrip()  # Remove whitespace
             if line:
-                line=re.sub('[^A-Za-z\d]+', '', line)     #Match all non alphanumeric characters and get rid of them
+                line=re.sub('[^A-Za-z\d]+', ' ', line)     #Match all non alphanumeric characters and get rid of them
                 line=line.lower()                         # Set line to lowercase
                 if len(line)>1:
                     writer.write(line+'\n')
 
 def main():
-    FILE_PATH = input("Please enter path of file to clean ")
+    # FILE_PATH = input("Please enter path of file to clean ")
 
     clean_data()
 
