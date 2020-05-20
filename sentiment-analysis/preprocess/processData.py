756807b51ec18223a48d8b0fd8549b2d848d685b
Lloyd Arnold <lloarn@gmail.com>
2020-05-20 13:57:35 +0100

    Add extra path constants

--- sentiment-analysis/preprocess/processData.py
+++ sentiment-analysis/preprocess/processData.py
@@ -11,9 +11,12 @@ CLEAN_POS = DATA_DIR + "processed/pos_clean"
 CLEAN_NEG = DATA_DIR + "processed/neg_clean"
 LAB_POS = DATA_DIR + "processed/pos_rev_lab"
 LAB_NEG = DATA_DIR + "processed/neg_rev_lab"
+PROCESSED_SET = DATA_DIR + "processed/all_reviews"
+
+TEST_SET = DATA_DIR + "processed/test"
+TRAIN_SET = DATA_DIR + "processed/train"
 
 WORD_MAP_PATH = DATA_DIR + "/processed/wordMap.json"
-
 
 def main():
     pass
