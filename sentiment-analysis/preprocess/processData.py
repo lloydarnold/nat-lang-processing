7241f380dff0470db832d85dca6aa9df4cd8e039
Lloyd Arnold <lloarn@gmail.com>
2020-05-20 13:52:45 +0100

    Add overarching process data script

---
+++ sentiment-analysis/preprocess/processData.py
@@ -0,0 +1,23 @@
+import cleanWords
+import buildWordMap
+import labelReviews
+import shuffle
+
+DATA_DIR = "/home/lloyd/Documents/programming/imperative/python/natlangprocessing/sentiment-analysis/data"
+
+RAW_POS = DATA_DIR + "raw/pos_reviews"
+RAW_NEG = DATA_DIR + "raw/neg_reviews"
+CLEAN_POS = DATA_DIR + "processed/pos_clean"
+CLEAN_NEG = DATA_DIR + "processed/neg_clean"
+LAB_POS = DATA_DIR + "processed/pos_rev_lab"
+LAB_NEG = DATA_DIR + "processed/neg_rev_lab"
+
+WORD_MAP_PATH = DATA_DIR + "/processed/wordMap.json"
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
