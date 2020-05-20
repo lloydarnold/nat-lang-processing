d5e76198295de0e3b5a8d3123cd260714d51731e
Lloyd Arnold <lloarn@gmail.com>
2020-05-20 14:11:16 +0100

    Write main body of processData

--- sentiment-analysis/preprocess/processData.py
+++ sentiment-analysis/preprocess/processData.py
@@ -1,7 +1,8 @@
-import cleanWords
-import buildWordMap
-import labelReviews
-import shuffle
+import cleanWords as cw
+import buildWordMap as bm
+import labelReviews as lr
+import shuffle as sh
+import splitSet as ss
 
 DATA_DIR = "/home/lloyd/Documents/programming/imperative/python/natlangprocessing/sentiment-analysis/data"
 
@@ -18,8 +19,23 @@ TRAIN_SET = DATA_DIR + "processed/train"
 
 WORD_MAP_PATH = DATA_DIR + "/processed/wordMap.json"
 
+
+def clean():
+    cw.clean_data(RAW_POS, CLEAN_POS)
+    cw.clean_data(RAW_NEG, CLEAN_NEG)
+
+
+def label():
+    lr.add_labels(CLEAN_NEG, LAB_NEG, 0)
+    lr.add_labels(CLEAN_POS, LAB_POS, 1)
+
+
 def main():
-    pass
+    clean()
+    bm.build_word_map([CLEAN_POS, CLEAN_NEG], WORD_MAP_PATH)
+    label()
+    sh.shuffle([LAB_POS, LAB_NEG], PROCESSED_SET)
+    ss.split_set(PROCESSED_SET, TEST_SET, TRAIN_SET, 200)
 
 
 if __name__ == '__main__':
