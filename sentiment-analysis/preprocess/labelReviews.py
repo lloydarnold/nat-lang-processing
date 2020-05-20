b56731474889ce14702fb2700b9f2a6c8e6ebe7d
Lloyd Arnold <lloarn@gmail.com>
2020-05-20 13:05:45 +0100

    Fix logic error in writing to labelled file

--- sentiment-analysis/preprocess/labelReviews.py
+++ sentiment-analysis/preprocess/labelReviews.py
@@ -14,12 +14,14 @@ def add_labels(filePath=None, labelledFilePath=None, label=None):
     for line in open(filePath, 'r'):
         line=line.strip("\n")
         line = line + (" | %d \n" %label)
+        outFile.write(line)
 
 
 def main():
     testPath1 = "/home/lloyd/Documents/programming/imperative/python/natlangprocessing/sentiment-analysis/data/test/testRaw1_clean"
-    testPath2 = testPath1 + _lab
+    testPath2 = testPath1 + "_lab"
     add_labels(testPath1, testPath2, 1)
+
 
 if __name__ == "__main__":
     main()
